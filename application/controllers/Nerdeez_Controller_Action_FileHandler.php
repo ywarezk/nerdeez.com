346584af46b34c863038411f97caa13813066652
Yariv <ceo@ykapps.com>
2012-11-10 11:53:12 +0200

    added download file to admin

--- application/controllers/Nerdeez_Controller_Action_FileHandler.php
+++ application/controllers/Nerdeez_Controller_Action_FileHandler.php
@@ -75,129 +75,20 @@ abstract class Nerdeez_Controller_Action_FileHandler extends Nerdeez_Controller_
      * @param String $sPath the path to the file to download
      */
     protected function download($sPath , $sTitle = ''){
-        //check file requested is readable
-        try{
-            $s3 = new Nerdeez_Service_Amazon_S3();
-            $data = $s3->getObject($sPath);
-        }
-        catch(Exception $e){
-            echo 'Failed reading file';
-            exit();
-        }
-        
-        //get the mime type and the size
-        $mime_type = NULL;
-        $size = 0;
+        //get the object info
+        $s3 = new Nerdeez_Service_Amazon_S3();
         $aObjectInfo = $s3 -> getInfo($sPath);
-        $size = $aObjectInfo['size'];
-        $mime_type = $aObjectInfo['type'];
 
-        //get file requested size
-        //$size = filesize($sPath);
-
-        //get the file name 
-        $name = rawurldecode($sTitle);
-
-        //table of possible mime types
-        /*$known_mime_types=array(
-                "pdf" => "application/pdf", 	
-                "zip" => "application/zip",
-                "doc" => "application/msword",
-                "xls" => "application/vnd.ms-excel",
-                "ppt" => "application/vnd.ms-powerpoint",
-                "gif" => "image/gif",
-                "png" => "image/png",
-                "jpeg"=> "image/jpg",
-                "docx"=>"application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-                "xlsx"=>"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-                "pptx"=>"application/vnd.openxmlformats-officedocument.presentationml.presentation",
-                "bmp"=>"image/bmp",
-                "jpg" =>  "image/jpg",
-                "c" => "text/plain",
-                "cpp" => "text/x-c",
-                "rar" => "application/x-rar-compressed",
-                "h" => "text/plain"
-         );
-
-         //get file extension
-        $file_extension = strtolower(substr(strrchr($sPath,"."),1));
-
-        //grab the right mime type from table and extension
-         if(array_key_exists($file_extension, $known_mime_types)){
-                $mime_type=$known_mime_types[$file_extension];
-         }
-         else{
-            echo 'Bad mime type';
-            exit();
-         }*/
-
-        @ob_end_clean(); //turn off output buffering to decrease cpu usage
-        // required for IE, otherwise Content-Disposition may be ignored
-        if(ini_get('zlib.output_compression'))
-                ini_set('zlib.output_compression', 'Off');
-
-        //set headers for download
-         header('Content-Type: ' . $mime_type);
-         
-         header('Content-Disposition: attachment; filename="'.$name.'"');
-         
-         header("Content-Transfer-Encoding: binary");
-         header('Accept-Ranges: bytes');
-         /* The three lines below basically make the 
-            download non-cacheable */
-         header("Cache-control: private");
-         header('Pragma: private');
-         header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
-
-
-          // multipart-download and download resuming support
-         if(isset($_SERVER['HTTP_RANGE']))
-         {
-                list($a, $range) = explode("=",$_SERVER['HTTP_RANGE'],2);
-                list($range) = explode(",",$range,2);
-                list($range, $range_end) = explode("-", $range);
-                $range=intval($range);
-                if(!$range_end) {
-                        $range_end=$size-1;
-                } else {
-                        $range_end=intval($range_end);
-                }
-
-                $new_length = $range_end-$range+1;
-                header("HTTP/1.1 206 Partial Content");
-                header("Content-Length: $new_length");
-                header("Content-Range: bytes $range-$range_end/$size");
-         } else {
-                $new_length=$size;
-                header("Content-Length: ".$size);
-         }
-
-         $file=$data;
-
-         /* output the file itself */
-         $chunksize = 1*(1024*1024); //you may want to change this
-         $bytes_send = 0;
-         if ($file)
-         {
-            if(isset($_SERVER['HTTP_RANGE']))
-            fseek($file, $range);
-
-            while(!feof($file) && 
-                    (!connection_aborted()) && 
-                    ($bytes_send<$new_length)
-                  )
-            {
-                    $buffer = fread($file, $chunksize);
-                    print($buffer); //echo($buffer); // is also possible
-                    flush();
-                    $bytes_send += strlen($buffer);
-            }
-            fclose($file);
-         } 
-         else {
-            return;
-         }
-        return;
+        if (is_array($aObjectInfo)) {
+            header('Content-type: ' . $aObjectInfo['type']);
+            header('Content-length: ' . $aObjectInfo['size']);
+            if ($sTitle !== '')
+                header('Content-Disposition: attachment; filename="'.rawurldecode($sTitle).'"');
+            echo $s3->getObject($sPath);
+        }
+        else {
+            header('HTTP/1.0 404 Not Found');
+        }
     }
     
     /**
