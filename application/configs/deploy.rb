7402468210970db7b7bbb518e4a4a2c3ca97ad45
Yariv <ceo@ykapps.com>
2012-12-02 19:55:27 +0200

    added deployment scripts

--- application/configs/deploy.rb
+++ application/configs/deploy.rb
@@ -1,20 +1,23 @@
 # general settings
 default_run_options[:pty] = true
-set :use_sudo, true
+set :use_sudo, false
  
 # source control settings
 set :scm, :git
-set :deploy_via, :remote_cache
-set :repository, "ssh://git@github.com:ywarezk/nerdeez.com.git"
+set :deploy_via, :export
+set :repository, "git@github.com:ywarezk/nerdeez.com.git"
+set :branch, "production"
  
 role :app, "nerdeez.com"
 role :web, "nerdeez.com"
-role :db, "nerdeez", :primary => true
+role :db, "nerdeez.com", :primary => true
  
 set :deploy_to, "/home/ywarezk2824/public_html/"
 
 set :user, "ywarezk2824"
 set :password, "KhruiNhe$%Vhpv"
+
+
 
 namespace :deploy do
  
