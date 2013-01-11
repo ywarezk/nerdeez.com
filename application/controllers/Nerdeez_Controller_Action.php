d30bcbea62f1d05454ecfa79d675dd539bfa85f6
Yariv <ceo@ykapps.com>
2013-01-11 09:21:19 +0200

    v1.1 - still trying to fix the mail issue

--- application/controllers/Nerdeez_Controller_Action.php
+++ application/controllers/Nerdeez_Controller_Action.php
@@ -377,6 +377,7 @@ abstract class Nerdeez_Controller_Action extends Zend_Controller_Action{
         $headers = 'MIME-Version: 1.0' . "\r\n";
         $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
         $headers .= 'From: admin@nerdeez.com' . "\r\n";
+        $body = wordwrap($body, 70, "\r\n");
         @mail($email,$title,$body,$headers);
     }
     
